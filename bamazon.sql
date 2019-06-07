09bbc8d8e1bdb3f9c0576a2392269e0f1dcee6cd
Anna rozymuradov <annarozymuradov@campus-075-028.ucdavis.edu>
2019-06-06 21:13:10 -0700

    nn

--- bamazon.sql
+++ bamazon.sql
@@ -16,14 +16,14 @@ CREATE TABLE products(
 Select * FROM products;
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) 
-VALUES (101, "Rims", "BMW", 79.99, 20),
-	   (212, "Break path", "Honda", 99.99, 10),
-	   (313, "Bumper", "Toyota", 29.99, 5),
-	   (420, "Wheel", "Jaguar", 129.99, 14),
-	   (504, "Window", "Ford", 39.99, 15),
-	   (619, "Window", "BMW", 19.99, 19),
-	   (720, "Wheel", "Honda", 49.99, 11),
-	   (808, "Key frame", "Mercedes Benz", 69.99, 10),
-	   (913, "Break path", "Nissan", 9.99, 19),
-	   (1009, "Window", "Nissan", 89.99, 17)
+VALUES (101, 'Rims', 'BMW', 79.99, 20),
+	   (212, 'Break path', 'Honda', 99.99, 10),
+	   (313, 'Bumper', 'Toyota', 29.99, 5),
+	   (420, 'Wheel', 'Jaguar', 129.99, 14),
+	   (504, 'Window', 'Ford', 39.99, 15),
+	   (619, 'Window', 'BMW', 19.99, 19),
+	   (720, 'Wheel', 'Honda', 49.99, 11),
+	   (808, 'Key frame', 'Mercedes Benz', 69.99, 10),
+	   (913, 'Break path', 'Nissan', 9.99, 19),
+	   (1009, 'Window', 'Nissan', 89.99, 17)
     
--- bamazonManager.js
+++ bamazonManager.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
 	host:"localhost",
 	port:3306,
 	user:"root",
-	password:"0323",
+	password:"Allamyrat1951",
 	database:"bamazon"
 });
 
